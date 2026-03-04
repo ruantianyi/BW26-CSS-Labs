7e844b89877948d5fda92f23e7cdbcb0b58c2e61
Tianyi Ruan <ruantianyi10@gmail.com>
2026-03-03 20:59:21 -0500

    Added another greeting to lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -10,7 +10,7 @@ def main():
 
     name = "Tianyi" # TODO: Insert your name between the double quotes
 
-    print(f"{name}, Welcome to the CSS course!")
+    print(f"{name}, Welcome to the CSS course! Hi hi hi hi hi")
 
 if __name__ == "__main__":
     main()
